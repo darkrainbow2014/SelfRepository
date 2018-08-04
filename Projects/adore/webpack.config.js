d069b5a58739c25734d34ec0145d8a97878991bf
tangqianchong <583395356@qq.com>
2018-08-04 12:33:41 +0800

    Add eslint support

--- Projects/adore/webpack.config.js
+++ Projects/adore/webpack.config.js
@@ -1,98 +1,98 @@
 const path = require('path');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin"); //for minimize js
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); //for minimize js
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: "development",
-	entry: ["./src/index.tsx"],
-	output: {
-		filename: "bundle.js",
-		path: __dirname + "/dist",
-		library: "adore"
-	},
+    mode: 'development',
+    entry: ['./src/index.tsx'],
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+        library: 'adore'
+    },
 
-	// Enable sourcemaps for debugging webpack's output.
-	devtool: "source-map",
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: 'source-map',
 
-	resolve: {
-		// Add '.ts' and '.tsx' as resolvable extensions.
-		extensions: [".ts", ".tsx", ".js", ".json"]
-	},
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
 
-	module: {
-		rules: [
-			// All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-			{
-				test: /\.tsx?$/,
-				loader: "awesome-typescript-loader"
-			},
-			// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-			{
-				enforce: "pre",
-				test: /\.js$/,
-				loader: "source-map-loader"
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					// "style-loader",
-					MiniCssExtractPlugin.loader,
-					{
-						loader: "css-loader",
-						options: {
-							sourceMap: true
-						}
-					},
-					{
-						loader: "sass-loader",
-						options: {
-							sourceMap: true,
-							// includePaths: ["absolute/path/a", "absolute/path/b"]
-						}
-					}
-				]
-			}
-		],
-	},
-	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new MiniCssExtractPlugin({
-			filename: "bundle.css"
-		}),
-		new HtmlWebpackPlugin({
-			title: 'Adore Single Page Application',
-			template: "index.html"
-		})
-	],
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				styles: {
-					name: 'styles',
-					test: /\.css$/,
-					chunks: 'all',
-					enforce: true
-				}
-			}
-		},
-		minimizer: [
-			new UglifyJsPlugin({
-				cache: true,
-				parallel: true,
-				sourceMap: true // set to true if you want JS source maps
-			}),
-			new OptimizeCSSAssetsPlugin({})
-		]
-	},
-	// When importing a module whose path matches one of the following, just
-	// assume a corresponding global variable exists and use that instead.
-	// This is important because it allows us to avoid bundling all of our
-	// dependencies, which allows browsers to cache those libraries between builds.
-	externals: {
-		"react": "React",
-		"react-dom": "ReactDOM"
-	}
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            // includePaths: ['absolute/path/a', 'absolute/path/b']
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Adore Single Page Application',
+            template: 'index.html'
+        })
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        },
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM'
+    }
 };
